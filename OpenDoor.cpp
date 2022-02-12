bdfab1a0f2c2cef42dcc28753ff84428d6e613ee
EpicMoroccanWarrior <97562298+EpicMoroccanWarrior@users.noreply.github.com>
2022-02-12 20:53:33 +0100

    Add files via upload

---
+++ OpenDoor.cpp
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+	InitialRotation = GetOwner()->GetActorRotation();
+	FinalRotation = InitialRotation;
+	if (!PressurePlate)
+	{
+		UE_LOG(LogTemp, Error, TEXT(" The OpenDoor Component attached to %s has no PressurePlate set!"), *GetOwner()->GetName());
+	}
+	
+	
+	ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
+		
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	if (PressurePlate && PressurePlate->IsOverlappingActor(ActorThatOpens))
+	{
+		OpenDoor(DeltaTime);
+	}else
+	{ 
+		CloseDoor(DeltaTime);
+	}
+	
+
+		
+	// else
+	// {
+	// 	FinalRotation.Yaw = FMath::FInterpTo(FinalRotation.Yaw,InitialRotation.Yaw,DeltaTime, 2);
+	// 	GetOwner()->SetActorRotation(FinalRotation);
+	// }
+}
+
+void UOpenDoor::OpenDoor(float DeltaTime){
+	FinalRotation.Yaw = FMath::FInterpTo(FinalRotation.Yaw,InitialRotation.Yaw + 90.f,DeltaTime, 2);
+	GetOwner()->SetActorRotation(FinalRotation);
+}
+void UOpenDoor::CloseDoor(float DeltaTime){
+	FinalRotation.Yaw = FMath::FInterpTo(FinalRotation.Yaw,InitialRotation.Yaw,DeltaTime, 3);
+	GetOwner()->SetActorRotation(FinalRotation);
+}
+
+
---
+++ OpenDoor.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
+#include "OpenDoor.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class MYPROJECT9_API UOpenDoor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOpenDoor();
+	UPROPERTY(EditAnywhere)
+	FRotator InitialRotation;
+	UPROPERTY(EditAnywhere)
+	FRotator FinalRotation;
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume* PressurePlate; 
+	UPROPERTY(EditAnywhere)
+	AActor* ActorThatOpens;
+	void OpenDoor(float DeltaTime);
+	void CloseDoor(float DeltaTime);
+
+	float DoorLastOpened = 0.f;
+	float DoorCloseDelay = 2.f;
+	
+
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
